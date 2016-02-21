047348a734ae54f7458a8a4579f57df23cca9964
JuanMa Ruiz <ruizjuanma@gmail.com>
2016-02-21 17:52:28 +0100

    Added grunt-contrib-clean, grunt-contrib-copy and created grunt:build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,16 +12,56 @@
                 },
                 dist: {
                     files: {
-                        'css/styles-demo.css': 'scss/main.scss'
+                        'dist/css/styles.css': 'assets/scss/main.scss'
                     }
+                }
+            },
+            copy: {
+                dist: {
+                    src: ['*.html', '!assests/**/*.css', '!assests/**/*.js'],
+                    dest: 'dist',
+                    expand: true
+                    
+                }
+            },
+            watch: {
+                files: ['assests/scss/*.scss', 'assests/js/*.js'],
+                tasks: ['sass', 'cssmin'],
+                options: {
+                    livereload: true
+                }
+            },
+            cssmin: {
+                target: {
+                    files: [{
+                        expand: true,
+                        cwd: 'dist/css',
+                        src: ['*.css', '!*.min.css'],
+                        dest: 'dist/css',
+                    }]
+                }
+            },
+            clean: {
+                build: {
+                    src: ['dist/']
                 }
             }
         });
 
         // Load plugins
         grunt.loadNpmTasks('grunt-sass');
+        grunt.loadNpmTasks('grunt-contrib-clean');
+        grunt.loadNpmTasks('grunt-contrib-copy');
+        grunt.loadNpmTasks('grunt-contrib-watch');
+        grunt.loadNpmTasks('grunt-contrib-cssmin');
 
         // Tasks
-        grunt.registerTask('default', ['sass']);
+        grunt.registerTask('build',[
+            'clean',
+            'sass',
+            'cssmin',
+            'copy'
+        ]);
+        grunt.registerTask('default', ['sass', 'cssmin']);
     }
 })()
